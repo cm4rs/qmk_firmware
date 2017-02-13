2eb1acf8f43eea2f35edeb1a9bba2bb1d1333a8f
Cyrille Mars - E2I <cyrille@eii.fr>
2017-02-13 14:36:02 +0100

    raccourcits pour ACTION_MODS_TAP_KEY
    cf quantum_keycodes.h

--- keyboards/planck/keymaps/m4rjou/keymap.c
+++ keyboards/planck/keymaps/m4rjou/keymap.c
@@ -68,10 +68,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-------------------------------------------------------------------------------------'
  */
 [_DVORAK_M] = {
-  {KC_TAB,                                KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,   KC_F,   KC_G,    KC_C,    KC_R,    KC_L,  KC_BSPC},
-  {ACTION_MODS_TAP_KEY(MOD_RCTL, KC_ESC), KC_A,    KC_O,    KC_E,    KC_U,    KC_I,   KC_D,   KC_H,    KC_T,    KC_N,    KC_S,  KC_ENT},
-  {KC_LSFT,                               KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,   KC_B,   KC_M,    KC_W,    KC_V,    KC_Z,  ACTION_MODS_TAP_KEY(MOD_RSFT, KC_SLSH) },
-  {BACKLIT,                               KC_LCTL, KC_LGUI, KC_LALT, LOWER_M, KC_SPC, KC_SPC, RAISE_M, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT}
+  {KC_TAB,        KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,   KC_F,   KC_G,    KC_C,    KC_R,    KC_L,  KC_BSPC},
+  {CTL_T(KC_ESC), KC_A,    KC_O,    KC_E,    KC_U,    KC_I,   KC_D,   KC_H,    KC_T,    KC_N,    KC_S,  KC_ENT},
+  {KC_LSFT,       KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,   KC_B,   KC_M,    KC_W,    KC_V,    KC_Z,  SFT_T(KC_SLSH) },
+  {BACKLIT,       KC_LCTL, KC_LGUI, KC_LALT, LOWER_M, KC_SPC, KC_SPC, RAISE_M, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT}
 },
 
 /* Dvorak jouff
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_DVORAK_J] = {
   {KC_ESC,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,  KC_Y,   KC_F,   KC_G,  KC_C,    KC_R,    KC_L,  KC_BSPC},
   {KC_TAB,  KC_A,    KC_O,    KC_E,    KC_U,  KC_I,   KC_D,   KC_H,  KC_T,    KC_N,    KC_S,  KC_ENT},
-  {KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,  KC_X,   KC_B,   KC_M,  KC_W,    KC_V,    KC_Z,  ACTION_MODS_TAP_KEY(MOD_RSFT, KC_SLSH)  },
+  {KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,  KC_X,   KC_B,   KC_M,  KC_W,    KC_V,    KC_Z,  SFT_T(KC_SLSH)  },
   {BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER_J, KC_SPC, KC_SPC, RAISE_J, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT}
 },
 
