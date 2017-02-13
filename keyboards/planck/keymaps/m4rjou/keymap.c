4c61d419f109bfe0ac2342c4432e2e9430719caf
Cyrille Mars <c@m4rs.org>
2017-02-13 23:23:49 +0100

    lower m4rs
    ajout des appostrophes françaises « »

--- keyboards/planck/keymaps/m4rjou/keymap.c
+++ keyboards/planck/keymaps/m4rjou/keymap.c
@@ -98,20 +98,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |   -  |   _  |   ?  |   =  |   +  |      |   (  |   )  |   {  |   }  |  |   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      | Undo | Cut  | Copy | Paste|      |      |      |      |      |      |      |
+ * |      | Undo | Cut  | Copy | Paste|      |      |   «  |   »  |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Home | PgUp | PgDn | End  |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER_M] = {
-  {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_EQL,  KC_DEL},
-  {_______, KC_MINS, KC_UNDS, KC_QUES, KC_EQL,  KC_PLUS, _______, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_PIPE},
-  {_______, W_UNDO,  W_CUT,   W_COPY,  W_PASTE, _______, _______, _______, _______, _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END}
+  {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_MINS, KC_EQL,  KC_DEL},
+  {_______, KC_MINS, KC_UNDS, KC_QUES, KC_EQL,  KC_PLUS, _______, KC_LPRN,    KC_RPRN,    KC_LCBR, KC_RCBR, KC_PIPE},
+  {_______, W_UNDO,  W_CUT,   W_COPY,  W_PASTE, _______, _______, UC(0x00ab), UC(0x00bb), _______, _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______,    KC_HOME,    KC_PGUP, KC_PGDN, KC_END}
 },
 
 /* Raise m4rs avec unicode directement, si ça marche on peut se servir d'un mapping qwerty
- * pour l'os plutôt que qwerty international
+ * pour l'os plutôt que qwerty international ce qui symplifie plein de choses
  * ,-----------------------------------------------------------------------------------.
  * |      |   ä  |   ê  |   ë  |   ü  |      |      |      |      |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
