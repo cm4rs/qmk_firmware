b73090cb66db63cf82aedc4bd6e5c5bcba1d922c
cdegouet <cedric.degouet@ntymail.com>
2017-02-19 16:16:47 +0100

    Dvorak jouff ajout touche Fn

--- keyboards/planck/keymaps/m4rjou/keymap.c
+++ keyboards/planck/keymaps/m4rjou/keymap.c
@@ -96,14 +96,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├──────┼──────┼──────┼──────┼───┄──┼──────╫──────┼───┄──┼──────┼──────┼──────┼──────┤
  * │ Shift│   ;  │   Q  │   J  │   K  │   X  ║   B  │   M  │   W  │   V  │   Z  │Shift │
  * ├──────┼──────┼──────┼──────┼──────┼──────╨──────┼──────┼──────┼──────┼──────┼──────┤
- * │ Brite│ Ctrl │ Alt  │ GUI  │Lower │    Space    │Raise │ Left │ Down │  Up  │Right │
+ * │ FN   │ Ctrl │ Alt  │ GUI  │Lower │    Space    │Raise │ Left │ Down │  Up  │Right │
  * └──────┴──────┴──────┴──────┴──────┴─────────────┴──────┴──────┴──────┴──────┴──────┘
  */
 [_DVORAK_J] = {
   {KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,  KC_Y,   KC_F,   KC_G,  KC_C,    KC_R,    KC_L,  KC_BSPC},
   {KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,  KC_I,   KC_D,   KC_H,  KC_T,    KC_N,    KC_S,  KC_ENT},
   {KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,  KC_X,   KC_B,   KC_M,  KC_W,    KC_V,    KC_Z,  KC_RSFT},
-  {BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER_J, KC_SPC, KC_SPC, RAISE_J, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT}
+  {FN01, KC_LCTL, KC_LALT, KC_LGUI, LOWER_J, KC_SPC, KC_SPC, RAISE_J, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT}
 },
 
 /* Lower m4rs
